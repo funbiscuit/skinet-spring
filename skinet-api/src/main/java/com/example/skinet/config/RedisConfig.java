cf2d9fa096c41d266f46cb65f1d82db47509bbfe
funbiscuit <kokurinsv@gmail.com>
2022-01-31 10:09:33 +0300

    Add configuration of redis host
    Add version to docker-compose file

--- skinet-api/src/main/java/com/example/skinet/config/RedisConfig.java
+++ skinet-api/src/main/java/com/example/skinet/config/RedisConfig.java
@@ -1,15 +1,20 @@
 package com.example.skinet.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+        return new JedisConnectionFactory(new RedisStandaloneConfiguration(redisHost));
     }
 
     @Bean
