b84dfd8325594aab8968a4aa4bd3885ebbf22543
funbiscuit <kokurinsv@gmail.com>
2022-02-02 12:51:32 +0300

    Enable CORS support in Spring Security

--- skinet-api/src/main/java/com/example/skinet/config/SecurityConfig.java
+++ skinet-api/src/main/java/com/example/skinet/config/SecurityConfig.java
@@ -39,7 +39,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.cors()
+                .and()
+                .csrf().disable()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
