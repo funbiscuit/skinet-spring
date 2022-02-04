44cb4ec136a243a7751f6e317cbec3e1fc93ce32
funbiscuit <kokurinsv@gmail.com>
2022-02-04 20:59:09 +0300

    Fix returning duplicate orders from API

--- skinet-api/src/main/java/com/example/skinet/repo/OrderRepository.java
+++ skinet-api/src/main/java/com/example/skinet/repo/OrderRepository.java
@@ -2,7 +2,19 @@ package com.example.skinet.repo;
 
 import com.example.skinet.core.entity.order.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface OrderRepository extends JpaRepository<Order, Integer>, OrderRepositoryCustom {
+import java.util.List;
+import java.util.Optional;
 
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+    @Query("select distinct o from Order o join fetch o.orderItems items " +
+            "join fetch o.deliveryMethod dm where o.buyerEmail = ?1 " +
+            "order by o.orderDate desc")
+    List<Order> getUserOrders(String email);
+
+    @Query("select o from Order o join fetch o.orderItems items " +
+            "join fetch o.deliveryMethod dm where o.buyerEmail = ?1 and o.id = ?2")
+    Optional<Order> getUserOrderById(String email, Integer id);
 }
